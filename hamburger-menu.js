adc20e6bc79bea9ba19958e8558dbd41d4f003de
parisbuedel <paris.buedel@colorado.edu>
2025-05-25 21:40:13 -0600

    Delete hamburger-menu.js

--- hamburger-menu.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  const burger = document.getElementById("hamburger-toggle");
-  const nav = document.querySelector("header nav");
-
-  burger.addEventListener("click", function() {
-    nav.classList.toggle("active");
-  });
-});
